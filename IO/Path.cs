b3d9d9cf26d7bc7c0d8c4087dbfa43c487048ad3
lybxlpsv <lybxlpsv@yahoo.com>
2019-03-09 21:23:50 +0700

    Initial commit

---
+++ IO/Path.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace mania4key_v2.IO
+{
+    class Path
+    {
+        public static string datapath = "/sdcard/lybmania4key/";
+        public static string audiopath = datapath + "audio/";
+        public static string sp = "/";
+        public static void CreateFolderIfMissing()
+        {
+            if (!Directory.Exists("/sdcard/lybmania4key"))
+            {
+                Directory.CreateDirectory("/sdcard/lybmania4key");
+                Directory.CreateDirectory("/sdcard/lybmania4key/sounds");
+            }
+        }
+    }
+}
